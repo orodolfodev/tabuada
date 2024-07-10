626139a84910ac3b2cb00081cfdab49fa571eab1
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-07-10 10:51:10 -0300

    Projeto tabuada finalzado

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,47 @@
-const myName = "Rodolfo";
-console.log(myName);
+// Seleção de elementos 
+const multiplicationFomr = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+// Funções
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+       
+        const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
+            <div class="result"> ${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parserFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+    }
+
+        multiplicationTitle.innerText = number; 
+};
+
+// Eventos
+
+multiplicationFomr.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+});
